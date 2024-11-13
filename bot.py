1a74e04e989ff22bb06940aaa067e8f6efa51998
kagaradata <137279126+kagaradata@users.noreply.github.com>
2024-11-13 13:29:47 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,45 +9,53 @@ bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(commands=['start'])
 def welcome(message):
 
+    # adding buttons in bot
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Рандомное число")
-    item2 = types.KeyboardButton("Как дела?")
+    item1 = types.KeyboardButton("rundom number")
+    item2 = types.KeyboardButton("how are you?")
 
     markup.add(item1, item2)
 
-    bot.send_message(message.chat.id, "добро пожаловать! я создан, чтобы надо мной проводили опыты...", reply_markup=markup)
+    bot.send_message(message.chat.id, "welcome!", reply_markup=markup)
 
+# skills
 @bot.message_handler(content_types=['text'])
 def lalala(message):
+    
     if message.chat.type == 'private':
-        if message.text == 'Рандомное число':
+        if message.text == 'random number':
             bot.send_message(message.chat.id, str(random.randint(0,100)))
-        elif message.text == 'Как дела?':
+        
+        elif message.text == 'how are you?':
 
             markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton("хорошо", callback_data='good')
-            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+            item1 = types.InlineKeyboardButton("good", callback_data='good')
+            item2 = types.InlineKeyboardButton("not good", callback_data='bad')
 
             markup.add(item1, item2)
 
-            bot.send_message(message.chat.id, 'Отлично, как сам?', reply_markup=markup)
+            bot.send_message(message.chat.id, 'right! and you?', reply_markup=markup)
 
         else:
-            bot.send_message(message.chat.id, 'Я не знаю, что ответить(...')
+            bot.send_message(message.chat.id, 'i dont know')
 
+
+# possible answer to our message
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     try:
         if call.message:
             if call.data == 'good':
-                bot.send_message(call.message.chat.id, 'Вот и отличненько)')
+                bot.send_message(call.message.chat.id, 'right!)')
             elif call.data == 'bad':
-                bot.send_message(call.message.chat.id, 'Не грусти..')
+                bot.send_message(call.message.chat.id, 'dont worry..')
 
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.chat.id, text="Как дела?",
+            # the disappearance of the buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.chat.id, text="how are you?",
                                   reply_markup=None)
 
-            bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text="Пробное уведомление")
+            # trial notification
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text="123")
 
     except Exception as e:
         print(repr(e))
